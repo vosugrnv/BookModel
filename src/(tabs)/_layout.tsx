59aec872e743cf795f5e912975a4b3c4a70e4041
vosugrnv <vosugrnv@gmail.com>
2026-05-06 09:36:40 +0700

    Create _layout.tsx

---
+++ src/(tabs)/_layout.tsx
@@ -0,0 +1,7 @@
+import AppTabs from '@/components/app-tabs';
+import React from 'react';
+
+/** Nhóm tab bar (Khám phá / Hoạt động / Tài khoản). Các màn push khác nằm ở layout Stack gốc. */
+export default function TabsGroupLayout() {
+  return <AppTabs />;
+}
